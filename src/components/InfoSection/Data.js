0cbd454d6f284f5ac97f84e500bae11b9865d5b0
Michael Moawad <michaelmoawad1997@gmail.com>
2020-10-26 18:39:48 +0000

    Original Website before changes

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,56 @@
+import ImageOne from "../../images/svg-1.svg"
+import ImageTwo from "../../images/svg-2.svg"
+import ImageThree from "../../images/svg-3.svg"
+import ImageFour from "../../images/svg-4.svg"
+
+
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: ImageOne,
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimited Access',
+    headline: 'Unlimited Transactions with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transactions without getting charged any fees.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: ImageTwo,
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headline: 'Creating an account is extremely easy',
+    description: 'Get everything set up and ready in under 10 minutes. All you need to do is add your information and you are ready to go.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: ImageThree,
+    alt: 'Paper',
+    dark: false,
+    primary: false,
+    darkText: true
+}
