9a3764e2511052ccb1fc5a2eb07b6d30cd6f915e
Michael Moawad <michaelmoawad1997@gmail.com>
2020-11-09 17:50:47 +0000

    test for re-deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,9 @@ import React, {useState} from 'react'
 import Footer from '../components/Footer'
 import HeroSection from '../components/HeroSection'
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjProfiles } from '../components/InfoSection/Data'
-<<<<<<< HEAD
-import InfoSection from '../components/InfoSection/index'
-import Stats from '../components/StatsSection'
-=======
 import About from '../components/InfoSection/About'
 import Mentorship from '../components/InfoSection/Mentorship'
 import Contact from '../components/InfoSection/Contact'
->>>>>>> sections-re-structure
 import Navbar from '../components/Navbar'
 import Profiles from '../components/Profiles'
 import Sidebar from '../components/Sidebar'
@@ -21,20 +16,13 @@ const Home = () => {
         setIsOpen(!isOpen)
     }
 
-
     return (
         <>
             <Sidebar isOpen={isOpen} toggle={toggle}/>
             <Navbar toggle={toggle}/>
             <HeroSection />
-<<<<<<< HEAD
-            <InfoSection {...homeObjOne}/>
-            <Stats />
-            <InfoSection {...homeObjTwo}/>
-=======
             <About {...homeObjOne}/>
             <Mentorship {...homeObjTwo}/>
->>>>>>> sections-re-structure
             <Profiles {...homeObjProfiles}/>
             <Contact {...homeObjThree}/>
             <Footer />
